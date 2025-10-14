05d71920a2152bd31535ebc2a876d2618bee5bdd
letago-ms <letagosekokotla01@gmail.com>
2025-10-14 16:11:00 +0200

    managed to create the user interfaces using html, now i'm busy with page navigations

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -12,16 +12,7 @@ namespace Contract_Monthly_Claim_System.Controllers
         [HttpPost]
         public IActionResult Index(string role, string FirstName, string LastName, string Email, string Password, string ConfirmPassword)
         {
-            // Basic validation
-            if (string.IsNullOrEmpty(role))
-            {
-                ViewBag.Error = "Please select your role.";
-                return View();
-            }
-
-            // Add your registration logic here (save to database, etc.)
-            // For now, we'll just redirect based on role
-
+            
             if (role == "Lecturer")
                 return RedirectToAction("Index", "LectureClaim");
             if (role == "Coordinator")
